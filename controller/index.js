cff2b13d4c99d47ad69955d2c98343ac17664185
James <zambranojamesj@gmail.com>
2022-05-15 14:41:21 -0300

    add post transaction route

--- controller/index.js
+++ controller/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3000;
-
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
@@ -18,16 +17,43 @@ connection.connect((error) => {
 
 //Routes
 app.get('/', (req, res) => {
-  res.send('Budget Manager App - Alkemy FullStack Challenge');
+  res.send("Budget Manager App - Alkemy FullStack Challenge");
 });
 
 app.get('/transactions', (req, res) => {
-  connection.query('SELECT * FROM `transactions`', (error, results) => {
+  connection.query("SELECT * FROM `transactions`", (error, results) => {
     if (error) throw error;
     console.log(results);
     res.json(results);
   });
 });
+
+app.post('/transactions', bodyParser.json(), (req, res) => {
+
+  const transaction = {
+    concept: req.body.concept,
+    amount: req.body.amount,
+    date: req.body.date,
+    type: req.body.type,
+    categoryId: req.body.categoryId
+  };
+
+  connection.query(`
+  INSERT INTO transactions (concept, amount, date, type, categoryId) 
+  VALUES (
+    "${transaction.concept}", 
+    ${transaction.amount}, 
+    "${transaction.date}", 
+    "${transaction.type}", 
+    ${transaction.categoryId})`,
+    (error, results) => {
+      if (error) throw error;
+      console.log(results);
+      res.json(results);
+    });
+});
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
