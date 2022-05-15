28baa81021cdec1613f7871e1f056e859d379bbe
James <zambranojamesj@gmail.com>
2022-05-15 11:40:30 -0300

    add db connection

--- controller/index.js
+++ controller/index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Kainner#777"
+});
+
+connection.connect((error) => {
+  if (error) throw error;
+  console.log("connection to DB established");
+});
+
+//Routes
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
