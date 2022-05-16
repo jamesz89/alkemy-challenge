6212c82a71c107a700c3fc4d4da601ac8cf37973
James <zambranojamesj@gmail.com>
2022-05-16 13:12:37 -0300

    extract routes as modules

--- api/app.js
+++ api/app.js
@@ -1,85 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mysqlConnection = require("./connection");
+const transactions = require("./routes/transactions");
 
 const app = express();
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Kainner#777",
-  database: "budget_manager"
-});
-
-connection.connect((error) => {
+mysqlConnection.connect((error) => {
   if (error) throw error;
   console.log("connection to DB established");
 });
 
-//Routes
-app.get('/', (req, res) => {
-  res.send("Budget Manager App - Alkemy FullStack Challenge");
-});
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/transactions', (req, res) => {
-  connection.query("SELECT * FROM `transactions`", (error, results) => {
-    if (error) throw error;
-    console.log(results);
-    res.json(results);
-  });
-});
-
-app.post('/transactions', bodyParser.json(), (req, res) => {
-  const transaction = {
-    concept: req.body.concept,
-    amount: req.body.amount,
-    date: req.body.date,
-    type: req.body.type,
-    categoryId: req.body.categoryId
-  };
-
-  connection.query(`
-  INSERT INTO transactions (concept, amount, date, type, categoryId) 
-  VALUES (
-    "${transaction.concept}", 
-    ${transaction.amount}, 
-    "${transaction.date}", 
-    "${transaction.type}", 
-    ${transaction.categoryId})`,
-    (error, results) => {
-      if (error) throw error;
-      console.log(results);
-      res.json(results);
-    });
-});
-
-app.delete('/transactions/:id', bodyParser.json(), (req, res) => {
-  const id = req.params.id;
-
-  connection.query(`DELETE FROM transactions WHERE id=${id};`, (error, results) => {
-    if (error) throw error;
-    console.log(results);
-    res.json(results);
-  });
-});
-
-app.put('/transactions/:id', bodyParser.json(), (req, res) => {
-  const id = req.params.id;
-  const body = req.body;
-
-  connection.query(`UPDATE transactions
-	SET concept="${body.concept}",
-  amount=${body.amount},
-  date="${body.date}",
-  type="${body.type}",
-  categoryId=${body.categoryId}
-	WHERE id=${id};`,
-    (error, results) => {
-      if (error) throw error;
-      console.log(results);
-      res.json(results);
-    });
-});
+app.use("/api/transactions", transactions);
 
 const PORT = 3000;
 
---
+++ api/routes/transactions.js
@@ -0,0 +1,64 @@
+const Router = require('express').Router();
+const connection = require("../connection");
+
+Router.get('/', (req, res) => {
+  connection.query("SELECT * FROM `transactions`", (error, results) => {
+    if (error) throw error;
+    console.log(results);
+    res.json(results);
+  });
+});
+
+Router.post('/', (req, res) => {
+  const transaction = {
+    concept: req.body.concept,
+    amount: req.body.amount,
+    date: req.body.date,
+    type: req.body.type,
+    categoryId: req.body.categoryId
+  };
+
+  connection.query(`
+  INSERT INTO transactions (concept, amount, date, type, categoryId) 
+  VALUES (
+    "${transaction.concept}", 
+    ${transaction.amount}, 
+    "${transaction.date}", 
+    "${transaction.type}", 
+    ${transaction.categoryId})`,
+    (error, results) => {
+      if (error) throw error;
+      console.log(results);
+      res.json(results);
+    });
+});
+
+Router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+
+  connection.query(`DELETE FROM transactions WHERE id=${id};`, (error, results) => {
+    if (error) throw error;
+    console.log(results);
+    res.json(results);
+  });
+});
+
+Router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+
+  connection.query(`UPDATE transactions
+	SET concept="${body.concept}",
+  amount=${body.amount},
+  date="${body.date}",
+  type="${body.type}",
+  categoryId=${body.categoryId}
+	WHERE id=${id};`,
+    (error, results) => {
+      if (error) throw error;
+      console.log(results);
+      res.json(results);
+    });
+});
+
+module.exports = Router;
